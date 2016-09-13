439aa7ac57ee57da9b124c9532e6365ec0b61ff4
nicholas.waun <nicholas.waun@L31410.ncfb.org>
2016-09-13 09:56:33 -0400

    Creatting app project for SW

---
+++ swapp/src/red/arpanet/sw/valueobjects/NewCharacter.java
@@ -0,0 +1,92 @@
+/*
+	©2016 ARPANET.RED
+	
+	All rights reserved.
+	
+	* No Warranty * 
+		1.1. "As-Is".
+			The Software is provided "as is,"
+			with all faults, defects and errors, and without
+			warranty of any kind.
+			
+		1.1. No Liability.
+			Licensor does not warrant that
+			the Software will be free of bugs, errors, viruses
+			or other defects, and Licensor shall have no
+			liability of any kind for the use of or inability
+			to use the software, the software content or any
+			associated service.
+*/
+
+package red.arpanet.sw.valueobjects;
+
+import java.util.Date;
+import java.util.Set;
+
+public class NewCharacter {
+
+	protected int id;
+	protected String name;
+	protected String desc;
+	protected Set<Attribute> attributes;
+	protected Set<Skill> skills;
+	protected Date creationDate;
+	protected Date updateDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Set<Attribute> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(Set<Attribute> attributes) {
+		this.attributes = attributes;
+	}
+
+	public Set<Skill> getSkills() {
+		return skills;
+	}
+
+	public void setSkills(Set<Skill> skills) {
+		this.skills = skills;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+}
